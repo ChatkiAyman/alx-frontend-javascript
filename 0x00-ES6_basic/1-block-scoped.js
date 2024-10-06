17ffc4e7cd20629755a81a6a430a293f1b706594
ChatkiAyman <aymanchatki@gmail.com>
2024-10-06 17:01:39 +0100

    Add comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+        /* eslint-disable no-unused-vars */
+        const task = true; // This task is scoped to the if block
+        const task2 = false; // This task2 is scoped to the if block
     }
   
     return [task, task2];
