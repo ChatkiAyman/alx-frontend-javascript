cedc58f65072bb2466d2c3a10dc051195ea9e05a
ChatkiAyman <aymanchatki@gmail.com>
2024-10-07 11:15:35 +0100

    Refactor code to use consistent indentation and spacing in taskBlock function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
   
-    if (trueOrFalse) {
-        /* eslint-disable no-unused-vars */
-        const task = true; // This task is scoped to the if block
-        const task2 = false; // This task2 is scoped to the if block
-    }
-  
-    return [task, task2];
+  if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
+    const task = true; // This task is scoped to the if block
+    const task2 = false; // This task2 is scoped to the if block
   }
+
+  return [task, task2];
+}
