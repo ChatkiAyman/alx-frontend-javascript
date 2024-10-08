505ce63685048a0fe4f4e4085ca723b8b992f13c
ChatkiAyman <aymanchatki@gmail.com>
2024-10-08 22:02:48 +0100

    Refactor code to update export statement in getStudentsByLocation and updateStudentGradeByCity functions

---
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -0,0 +1,11 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+    return students.filter((student) => student.location === city)
+        .map((student) => {
+        const grades = newGrades.filter((grade) => grade.studentId === student.id);
+        // return object with updated grade
+        return {
+            ...student,
+            grade: grades.length ? grades[0].grade : 'N/A',
+        };
+    });
+} export default updateStudentGradeByCity;
