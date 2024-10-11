7663fcd6444ba0ed6152308acbd99b8a1e172769
ChatkiAyman <aymanchatki@gmail.com>
2024-10-11 10:09:24 +0100

    Refactor cleanSet function to handle empty startString input

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,19 @@
+/**
+ * Cleans a set by filtering out values that do not start with
+ * a given string and returns a hyphen-separated string of the remaining values.
+ *
+ * @param {Set} set - The set to be cleaned.
+ * @param {string} startString - The string that
+ * the values in the set should start with.
+ * @returns {string} - A hyphen-separated string of
+ * the values in the set that start with the given string.
+ */
 function cleanSet(set, startString) {
-  if (startString === '') return '';
-
-  const result = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
-    }
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
-
-  return result.join('-');
-}
-
-export default cleanSet;
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+} export default cleanSet;
