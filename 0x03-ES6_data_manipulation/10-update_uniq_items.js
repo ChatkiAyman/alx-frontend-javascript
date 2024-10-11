2264a0c9d2460ccf8000fa94c4db53ce810a8d85
ChatkiAyman <aymanchatki@gmail.com>
2024-10-11 09:48:04 +0100

    Refactor updateUniqueItems function to update the value of the key Pasta and Rice to 100

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('The first argument must be a Map instance.');
+  }
+  /*
+      the code below is to update
+      the value of the key Pasta and Rice to 100 */
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
+
+export default updateUniqueItems;
